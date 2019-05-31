98fd3fc415bff24609d27c960a90009a9843e9e2
Jacob Brunsting <jacob.brunsting@gmail.com>
2019-05-30 23:04:39 -0400

    Remove user name from user

--- server.py
+++ server.py
@@ -10,20 +10,21 @@ from urllib.parse import parse_qs
 
 from passlib.hash import pbkdf2_sha256
 
+r = 200000
 d = 'utf-8'
 us = {}
 
 for n in os.listdir('.'):
     if not n.endswith('.u'): continue
     f = open(n[:-2] + '.u', 'r')
-    u = [n[:-2], f.readline()[:-1], [], {}]
-    us[u[0]] = u
+    u = [f.readline()[:-1], [], {}]
+    us[n[:-2]] = u
     for i in range(int(f.readline())):
         s = f.readline()[:-1].split(',', 1)
-        u[2].append((s[0].replace('\a', '\n'), float(s[1])))
+        u[1].append((s[0].replace('\a', '\n'), float(s[1])))
     for i in range(int(f.readline())):
         e = f.readline()[:-1].split(',', 2)
-        u[3][e[1]] = [e[1], e[2], e[0] == 'True']
+        u[2][e[1]] = [e[1], e[2], e[0] == 'True']
 
 
 class H(BaseHTTPRequestHandler):
@@ -37,7 +38,7 @@ class H(BaseHTTPRequestHandler):
             if a in us:
                 u = us[a]
                 timestamp = time.time()
-                if cs['b'].value in [s[0] for s in u[2] if s[1] > timestamp]:
+                if cs['b'].value in [s[0] for s in u[1] if s[1] > timestamp]:
                     return u
         return None
 
@@ -65,8 +66,7 @@ class H(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         if not u:
-            self.render_home()
-            return
+            return self.render_home()
 
         self.write_html('''
         <style>
@@ -91,10 +91,9 @@ class H(BaseHTTPRequestHandler):
             </div>
         </label>
         <ul style="padding:0">''' + ''.join([
-            '<li style="display:block;border-top:1px solid black;">' + '''
-                <form style="display:inline-block" action="/e_t/''' + e[0] +
-            '''" method="post">
-                    <input style="background:none;border:none;cursor:pointer" type="submit" value="&#974'''
+            '<li style="display:block;border-top:1px solid black;"><form style="display:inline-block" action="/e_t/'
+            + e[0] +
+            '" method="post"><input style="background:none;border:none;cursor:pointer" type="submit" value="&#974'
             + str(4 + int(e[2])) + '''">
                 </form>
                 <p style="display:inline-block;width:calc(100% - 100px);overflow-wrap:break-word;text-decoration:'''
@@ -103,17 +102,17 @@ class H(BaseHTTPRequestHandler):
             + e[0] + '''" method="post">
                     <input style="background:none;border:none;cursor:pointer" type="submit" value='x'>
                 </form>
-                ''' + '</li>' for e in u[3].values()
+                ''' + '</li>' for e in u[2].values()
         ]) + '''</ul>
         <form action="/n" method="post">
             <input style="width:100%;margin-right:-45px;padding-right:45px" type="text" name="l">
             <input style="width:35px;padding:0;margin:0;cursor:pointer;background:none;border:none" type="submit" value="+">
         </form>''')
 
-    def create_session_cookie(self, u):
+    def create_session_cookie(self, a):
         b = secrets.token_urlsafe()
-        u[2].append((b, time.time() + 7**8))
-        c, c['a'], c['b'] = SimpleCookie(), u[0], b
+        us[a][1].append((b, time.time() + 7**8))
+        c, c['a'], c['b'] = SimpleCookie(), a, b
         return c
 
     def do_POST(self):
@@ -126,8 +125,8 @@ class H(BaseHTTPRequestHandler):
         if self.path == '/Login':
             if a in us:
                 u = us[a]
-                if pbkdf2_sha256.verify(b, us[a][1]):
-                    c = self.create_session_cookie(us[a])
+                if pbkdf2_sha256.verify(b, us[a][0]):
+                    c = self.create_session_cookie(a)
                 else:
                     err = (401, 'Username or password incorrect')
             else:
@@ -136,33 +135,32 @@ class H(BaseHTTPRequestHandler):
             if a in us:
                 err = (400, 'Username \'' + a + '\' already taken')
             else:
-                pass_hash = pbkdf2_sha256.encrypt(
-                    b, rounds=200000, salt_size=16)
-                us[a] = [a, pass_hash, [], {}]
-                u = us[a]
-                c = self.create_session_cookie(us[a])
+                us[a] = [
+                    a,
+                    pbkdf2_sha256.encrypt(b, rounds=r, salt_size=16), [], {}
+                ]
+                u, c = us[a], self.create_session_cookie(a)
         elif self.path == '/l':
             c, c['a'], c['b'] = SimpleCookie(), '', ''
         elif self.path == '/r':
             if u:
-                if pbkdf2_sha256.verify(body[b'b'][0].decode(), u[1]):
-                    u[1] = pbkdf2_sha256.encrypt(
-                        body[b'c'][0].decode(d), rounds=200000, salt_size=16)
-                    u[2] = []
+                if pbkdf2_sha256.verify(body[b'b'][0].decode(), u[0]):
+                    u[0] = pbkdf2_sha256.encrypt(
+                        body[b'c'][0].decode(d), rounds=r, salt_size=16)
+                    u[1] = []
                 else:
                     self.send_response(401)
                     self.end_headers()
-                    self.wfile.write(b'<p>Password incorrect</p>')
-                    return
+                    return self.wfile.write(b'<p>Password incorrect</p>')
         elif self.path == '/n':
             if u:
                 id = str(uuid.uuid4())
-                u[3][id] = [id, body[b'l'][0].decode(d), False]
+                u[2][id] = [id, body[b'l'][0].decode(d), False]
         elif self.path.startswith('/e_t') or self.path.startswith('/e_d'):
             id = self.path.split('/', 2)[2]
-            u[3][id][2] = not u[3][id][2]
+            u[2][id][2] = not u[2][id][2]
             if self.path.startswith('/e_d'):
-                del u[3][id]
+                del u[2][id]
         else:
             err = (404, '')
 
@@ -171,16 +169,16 @@ class H(BaseHTTPRequestHandler):
             self.end_headers()
             self.render_home(err[1])
         else:
-            f = open(u[0] + '.u', 'w+')
-            f.write(u[1] + '\n')
+            f = open(a + '.u', 'w+')
+            f.write(u[0] + '\n')
             timestamp = time.time()
-            u[2] = [s for s in u[2] if s[1] > timestamp]
+            u[1] = [s for s in u[1] if s[1] > timestamp]
             f.write(
-                str(len(u[2])) + '\n' + ''.join(
+                str(len(u[1])) + '\n' + ''.join(
                     s[0].replace('\n', '\a') + ',' + str(s[1]) + '\n'
-                    for s in u[2]) + str(len(u[3].values())) + '\n' + ''.join(
+                    for s in u[1]) + str(len(u[2].values())) + '\n' + ''.join(
                         str(e[2]) + ',' + e[0] + ',' + e[1] + '\n'
-                        for e in u[3].values()))
+                        for e in u[2].values()))
             self.send_response(301)
             if c:
                 self.send_header('Set-Cookie', c.output(header='', sep=''))
