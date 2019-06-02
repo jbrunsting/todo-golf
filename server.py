972547c0b39a4c30269defe09120d6eccc1174a3
Jacob Brunsting <jacob.brunsting@gmail.com>
2019-06-02 13:26:30 -0400

    Below 6k bytes

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
+import http.server
 import os
 import secrets
 import time
 import uuid
 from http.cookies import SimpleCookie
-from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qs
 
-from passlib.hash import pbkdf2_sha256
+from passlib.hash import pbkdf2_sha256 as sha
 
 r = 200000
 d = 'utf-8'
@@ -26,6 +26,7 @@ for n in os.listdir('.'):
         e = f.readline()[:-1].split(',', 2)
         u[2][e[1]] = [e[2], e[0] == 'True']
 
+
 def get_u(s):
     cs = SimpleCookie(s.headers.get('Cookie'))
     if 'a' in cs and 'b' in cs:
@@ -35,13 +36,15 @@ def get_u(s):
             timestamp = time.time()
             if cs['b'].value in [s[0] for s in u[1] if s[1] > timestamp]:
                 return (u, a)
-    return (None, None)
+    return ('', '')
+
 
 def w(s, b):
     return s.wfile.write(
         bytes(
             '<title>TD</title><body style="max-width:800px;margin:auto;padding:16px">'
             + b + '</body>', d))
+
 
 def h(s, error=None):
     form = '''
@@ -52,9 +55,9 @@ def h(s, error=None):
         <input style="display:block" type="password" name="b">
         <input type="submit" value="Signup">
     </form>'''
-    w(s, '<h2>TODO</h2>' + (
-        '<p>' + error + '</p>' if error else ''
-    ) + form + form.replace('left', 'right').replace('Signup', 'Login'))
+    w(s, '<h2>TODO</h2>' + ('<p>' + error + '</p>' if error else '') + form +
+      form.replace('left', 'right').replace('Signup', 'Login'))
+
 
 def g(s):
     u, a = get_u(s)
@@ -105,21 +108,22 @@ def g(s):
         <input style="display:inline-block;cursor:pointer;background:none;border:none" type="submit" value="+">
     </form>''')
 
+
 def create_session_cookie(a):
     b = secrets.token_urlsafe()
     us[a][1].append((b, time.time() + 7**8))
     c, c['a'], c['b'] = SimpleCookie(), a, b
     return c
 
+
 def p(s):
-    body, u, p = parse_qs(
-        s.rfile.read(int(
-            s.headers.get('Content-Length')))), get_u(s), s.path
+    body, u, p = parse_qs(s.rfile.read(int(
+        s.headers.get('Content-Length')))), get_u(s), s.path
     u, n = u
     a, b = [body.get(c, [b''])[0].decode(d) for c in [b'a', b'b']]
     c = err = None
     if p == '/Login':
-        if a in us and pbkdf2_sha256.verify(b, us[a][0]):
+        if a in us and sha.verify(b, us[a][0]):
             u, c = us[a], create_session_cookie(a)
         else:
             err = 'Username or password incorrect'
@@ -127,16 +131,13 @@ def p(s):
         if a in us:
             err = 'Username %s already taken' % (a)
         else:
-            us[a] = [
-                pbkdf2_sha256.encrypt(b, rounds=r, salt_size=16), [], {}
-            ]
+            us[a] = [sha.encrypt(b, rounds=r, salt_size=16), [], {}]
             u, c = us[a], create_session_cookie(a)
     elif p == '/l':
         c, c['a'], c['b'] = SimpleCookie(), '', ''
     if u and p == '/r':
-        if pbkdf2_sha256.verify(body[b'b'][0].decode(), u[0]):
-            u[0] = pbkdf2_sha256.encrypt(
-                body[b'c'][0].decode(d), rounds=r, salt_size=16)
+        if sha.verify(body[b'b'][0].decode(), u[0]):
+            u[0] = sha.encrypt(body[b'c'][0].decode(d), rounds=r, salt_size=16)
             u[1] = []
         else:
             s.send_response(401)
@@ -173,10 +174,7 @@ def p(s):
         s.end_headers()
 
 
-class H(BaseHTTPRequestHandler):
-    def __init__(s, *args, **kwargs):
-        super(H, s).__init__(*args, **kwargs)
-
+class H(http.server.BaseHTTPRequestHandler):
     def do_GET(s):
         g(s)
 
@@ -184,4 +182,4 @@ class H(BaseHTTPRequestHandler):
         p(s)
 
 
-HTTPServer(('localhost', 8080), H).serve_forever()
+http.server.HTTPServer(('localhost', 8080), H).serve_forever()
