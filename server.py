fbedce3409374b3afeb70e231130dc2e74b7ebb7
Jacob Brunsting <jacob.brunsting@gmail.com>
2019-06-08 13:59:21 -0400

    Pretty small

--- server.py
+++ server.py
@@ -27,39 +27,43 @@ for n in [f for f in os.listdir('.') if f.endswith('.u')]:
 
 
 def z(s):
-    cs = SimpleCookie(s.headers.get('Cookie'))
-    if 'a' in cs and 'b' in cs:
-        a = cs['a'].value
-        if a in us:
-            u = us[a]
-            if cs['b'].value in [s[0] for s in u[1] if s[1] > time.time()]:
-                return (u, a)
+    c = SimpleCookie(s.headers.get('Cookie'))
+    if 'a' in c and 'b' in c:
+        a = c['a'].value
+        if a in us and c['b'].value in [
+                s[0] for s in us[a][1] if s[1] > time.time()
+        ]:
+            return (us[a], a)
     return ('', '')
 
 
 def w(s, b):
     return s.wfile.write(
         bytes(
-            c('<title>TODO</title><body]max-width:800px;margin:auto;padding:16px">'
+            c('<title>TODOZtitle><body]max-width:800px;margin:auto;padding:16px">'
               ) + b + '</body>', d))
 
 
-def h(s, error=None):
-    form = c(
-        ')uSignup"]float:left">Username[text"]display:block" name="a">P!b"]display:block">(value="Signup"></form>'
+def h(s, e=None):
+    f = c(
+        '_uSignup"]float:left">Username[text"]display:block" name="a">P!b"]display:block">?value="Signup">Zform>'
     )
-    w(s, '<h2>TODO</h2>' + ('<p>' + error + '</p>' if error else '') + form +
-      form.replace('left', 'right').replace('Signup', 'Login'))
+    w(s, '<h2>TODO</h2>' + ('<p>%s</p>' % e if e else '') + f +
+      f.replace('left', 'right').replace('Signup', 'Login'))
 
 
 def c(s, b=0):
     if b > 5: return s
-    return s.replace('^', 'display:inline-block;').replace(
-        '@', 'background:none;border:none;cursor:pointer').replace(
-            '(', c('[submit" ', b + 1)).replace(
-                ')', '<form method="post" action="/').replace(
-                    '!', c('assword[password" name="', b + 1)).replace(
-                        '[', '<input type="').replace(']', ' style="')
+    p = [
+        '^', 'display:inline-block;', '@',
+        'background:none;border:none;cursor:pointer', '?',
+        c('[submit" ', b + 1), '_', '<form method="post" action="/', '!',
+        c('assword[password" name="',
+          b + 1), '[', '<input type="', ']', ' style="', 'Z', '</'
+    ]
+    for i in range(8):
+        s = s.replace(p[i * 2], p[i * 2 + 1])
+    return s
 
 
 def r(s, c):
@@ -74,37 +78,16 @@ def g(s):
         return h(s)
 
     w(s,
-      c('''
-<style>
-input:checked~div{display: block}
-div{display:none}
-input{display:block}
-</style>
-<label>
-[checkbox"]display:none"/>
-<h2]^margin:0">TODO</h2>
-<p]cursor:pointer;float:right;margin:8px">&#9881</p>
-<div>
-)l">
-(style="float:right" value="Logout">
-</form> 
-)r">
-P!b">
-New p!c">
-(value="Reset password">
-</form> 
-</div>
-</label>
-<ul]padding:0">''') + ''.join([
-          c('<li]display:block;border-top:1px solid black;">)e_t/') + k +
-          c('"]^">(style="@" value="&#974') + str(4 + int(e[1])) +
-          c('"></form><p]^width:calc(100% - 100px);overflow-wrap:break-word;text-decoration:'
-            ) + ('line-through'
-                 if e[1] else '') + '">' + e[0] + c('</p>)e_d/') + k +
-          c('"]^float:right;margin:16px">(style="@" value="x"></form>') +
-          '</li>' for k, e in u[2].items()
-      ]) +
-      c('</ul>)n">[text"]^width:100%;margin-right:-45px;padding-right:45px" name="l">(style="^@" value="+"></form>'
+      c('<style>input:checked~div{display:block}div{display:none}input{display:block}Zstyle><label>[checkbox"]display:none"/><h2]^margin:0">TODOZh2><p]cursor:pointer;float:right;margin:8px">&#9881Zp><div>_l">?style="float:right" value="Logout">Zform>_r">P!b">New p!c">?value="Reset password">Zform> Zdiv>Zlabel><ul]padding:0">'
+        ) +
+      ''.join(
+          c('<li]display:block;border-top:1px solid black;">_et/') + k +
+          c('"]^">?style="@" value="&#974') + str(4 + int(e[1])) +
+          c('">Zform><p]^width:calc(100% - 100px);overflow-wrap:break-word;text-decoration:'
+            ) + ('line-through' if e[1] else '') + '">' + e[0] + c('Zp>_ed/') +
+          k + c('"]^float:right;margin:16px">?style="@" value="x">Zform>') +
+          '</li>' for k, e in u[2].items()) +
+      c('Zul>_n">[text"]^width:100%;margin-right:-45px;padding-right:45px" name="l">?style="^@" value="+">Zform>'
         ))
 
 
@@ -121,10 +104,9 @@ def p(s):
     u, n = u
     a, b, i = [body.get(c, [b''])[0].decode(d) for c in [b'a', b'b', b'c']]
     c = err = None
-    if p[1:3] == 'uL':
-        if a not in us or not sha.verify(b, us[a][0]):
-            err = 'Username or password incorrect'
-    elif p[1:2] == 'u':
+    if p[1:2] == 'uL' and (a not in us or not sha.verify(b, us[a][0])):
+        err = 'Username or password incorrect'
+    if p[1:2] == 'uS':
         if a in us:
             err = 'Username %s already taken' % (a)
         else:
@@ -142,28 +124,24 @@ def p(s):
     if u and p == '/n':
         id = str(uuid.uuid4())
         u[2][id] = [body[b'l'][0].decode(d), False]
-    t = p[3:4]
-    if u and p[1:3] == 'e_':
+    if u and p[1:2] == 'e':
         id = p.split('/', 2)[2]
         u[2][id][1] = not u[2][id][1]
-        if t == 'd':
+        if p[2:3] == 'd':
             del u[2][id]
     if err:
         r(s, 200)
-        h(s, err)
-        return
+        return h(s, err)
     f = open((a or n) + '.u', 'w+')
     f.write(u[0] + '\n')
     u[1] = [s for s in u[1] if s[1] > time.time()]
-    f.write(
-        str(len(u[1])) + '\n' + ''.join(
-            s[0].replace('\n', '\a') + ',' + str(s[1]) + '\n'
-            for s in u[1]) + str(len(u[2].values())) + '\n' + ''.join(
-                str(e[1]) + ',' + k + ',' + e[0] + '\n'
-                for k, e in u[2].items()))
+    f.write('%s\n%s%s\n%s' %
+            (len(u[1]), ''.join(
+                '%s,%s\n' % (s[0].replace('\n', '\a'), s[1]) for s in u[1]),
+             len(u[2].values()),
+             ''.join('%s,%s,%s\n' % (e[1], k, e[0]) for k, e in u[2].items())))
     s.send_response(301)
-    if c:
-        s.send_header('Set-Cookie', c.output(header='', sep=''))
+    c and s.send_header('Set-Cookie', c.output(header='', sep=''))
     s.send_header('Location', '/')
     s.end_headers()
 
