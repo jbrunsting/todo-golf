326f04b4cd7f91f73639452b81e022e7fb9cd766
Jacob Brunsting <jacob.brunsting@gmail.com>
2019-06-08 20:28:48 -0400

    Last formatted version

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ import uuid
 from http.cookies import SimpleCookie as v
 from urllib.parse import parse_qs
 
-from passlib.hash import pbkdf2_sha256 as sha
+from passlib.hash import pbkdf2_sha256 as q
 
 k = 200000
 d = 'utf-8'
@@ -38,25 +38,25 @@ def z(s):
 
 
 w = lambda s, b: s.wfile.write(bytes(
-        c('<title>TODOZtitle><body]max-width:800px;W:auto;padding:16px">%s</body>'
-          % b), d))
+    c('<title>TODOZtitle><body]max-width:800px;W:auto;padding:16px">%sZbody>'
+    % b), d))
 
 
 def c(s, b=0):
     if b > 5:
         return s
     for e in zip(
-            '[^@?_!]ZYXMQAWBGIFV',
-            c('<I typeY|Finline-block;|background:none;border:none;M|[submit"|<X methodYpost" actionY/|Q[pQ" nameY| GY|</|="|form|cursor:pointer|assword|Username|margin|right|style|input|display:|Signup',
+            '[^@?_!]ZYXMQAWBGIFVJKC',
+            c('<I typeY|Finline-block;|background:none;border:none;M|[submit"|<X methodYpost" actionY/|Q[pQ" JY| GY|</|="|form|cursor:pointer|assword|UserJ|margin|right|style|input|display:|Signup|name|%s|value',
               b + 1).split('|')):
         s = s.replace(*e)
     return s
 
 
-f = c('_uV"]float:left">A[text"]Fblock" nameYa">P!b"]Fblock">?valueYV">ZX>')
+f = c('_uV"]float:left">A[text"]Fblock" JYa">P!b"]Fblock">?CYV">ZX>')
 
 
-h = lambda s, e='' : w(s, '<h2>TODO</h2><p>%s</p>%s%s' % (e, f, f.replace('left', c('B')).replace(c('V'), 'Login')))
+h = lambda s, e='' : w(s, c('<h2>TODOZh2><p>KZp>KK') % (e, f, f.replace('left', c('B')).replace(c('V'), 'Login')))
 
 
 def r(s, c, k=''):
@@ -73,21 +73,14 @@ def g(s):
         return h(s)
 
     w(s,
-      c('<G>I:checked~div{Fblock}div{Fnone}I{Fblock}ZG><label>[checkbox"]Fnone"/><h2]^W:0">TODOZh2><p]M;float:B;W:8px">&#9881Zp><div>_l">?GYfloat:B" valueYLogout">ZX>_r">P!b">New p!c">?valueYReset pQ">ZX> Zdiv>Zlabel><ul]padding:0">'
+      c('<G>I:checked~div{Fblock}div{Fnone}I{Fblock}ZG><label>[checkbox"]Fnone"/><h2]^W:0">TODOZh2><p]M;float:B;W:8px">&#9881Zp><div>_l">?GYfloat:B" CYLogout">ZX>_r">P!b">New p!c">?CYReset pQ">ZX> Zdiv>Zlabel><ul]padding:0">'
         ) +
       ''.join(
-          c('<li]Fblock;border-top:1px solid black;">_et/%s"]^">?GY@" valueY&#974%s">ZX><p]^width:calc(100%% - 100px);overflow-wrap:break-word;text-decoration:%s">%sZp>_ed/%s"]^float:B;W:16px">?GY@" valueYx">ZX>Zli>'
+          c('<li]Fblock;border-top:1px solid black;">_et/K"]^">?GY@" CY&#974K">ZX><p]^width:calc(100%% - 100px);overflow-wrap:break-word;text-decoration:K">KZp>_ed/K"]^float:B;W:16px">?GY@" CYx">ZX>Zli>'
             ) % (k, 4 + int(e[1]), e[1] and 'line-through', e[0], k)
           for k, e in u[2].items()) +
-      c('Zul>_n">[text"]^width:100%;W-B:-45px;padding-B:45px" nameYl">?GY^@" valueY+">ZX>'
+      c('Zul>_n">[text"]^width:100%;W-B:-45px;padding-B:45px" JYl">?GY^@" CY+">ZX>'
         ))
-
-
-def x(a):
-    b = secrets.token_urlsafe()
-    us[a][1].append((b, time.time() + 7**8))
-    c, c['a'], c['b'] = v(), a, b
-    return c
 
 
 def p(s):
@@ -96,26 +89,27 @@ def p(s):
     u, n = u
     a, b, i = [o.get(y, [b''])[0].decode(d) for y in [b'a', b'b', b'c']]
     y = err = None
-    if p[1:3] == 'uL' and (a not in us or not sha.verify(b, us[a][0])):
+    if p[1:3] == 'uL' and (a not in us or not q.verify(b, us[a][0])):
         err = c('A or pQ incorrect')
     if p[1:3] == 'uS':
         if a in us:
             err = 'A %s already taken' % (a)
         else:
-            us[a] = [sha.encrypt(b, rounds=k, salt_size=16), [], {}]
+            us[a] = [q.encrypt(b, rounds=k, salt_size=16), [], {}]
     if (p[1:3] == 'uL' or p[1:3] == 'uS') and not err:
-        u, y = us[a], x(a)
+        t = secrets.token_urlsafe()
+        us[a][1].append((t, time.time() + 7**8))
+        y, y['a'], y['b'], u = v(), a, t, us[a]
     if p == '/l':
         y, y['a'], y['b'] = v(), '', ''
     if u and p == '/r':
-        if not sha.verify(b, u[0]):
+        if not q.verify(b, u[0]):
             r(s, 401)
-            return w(s, c('<p>PQ incorrect</p>'))
-        u[0] = sha.encrypt(i, rounds=k, salt_size=16)
+            return w(s, c('<p>PQ incorrectZp>'))
+        u[0] = q.encrypt(i, rounds=k, salt_size=16)
         u[1] = []
     if u and p == '/n':
-        id = str(uuid.uuid4())
-        u[2][id] = [o[b'l'][0].decode(d), False]
+        u[2][str(uuid.uuid4())] = [o[b'l'][0].decode(d), False]
     if u and p[1:2] == 'e':
         id = p.split('/', 2)[2]
         u[2][id][1] = not u[2][id][1]
@@ -127,11 +121,12 @@ def p(s):
     f = open((a or n) + '.u', 'w+')
     f.write(u[0] + '\n')
     u[1] = [s for s in u[1] if s[1] > time.time()]
-    f.write('%s\n%s%s\n%s' %
-            (len(u[1]), ''.join(
-                '%s,%s\n' % (s[0].replace('\n', '\a'), s[1]) for s in u[1]),
-             len(u[2].values()),
-             ''.join('%s,%s,%s\n' % (e[1], k, e[0]) for k, e in u[2].items())))
+    f.write(
+        c('K\nKK\nK') %
+        (len(u[1]),
+         ''.join('%s,%s\n' % (s[0].replace('\n', '\a'), s[1]) for s in u[1]),
+         len(u[2].values()), ''.join(
+             '%s,%s,%s\n' % (e[1], k, e[0]) for k, e in u[2].items())))
     r(s, 301, y)
 
 
